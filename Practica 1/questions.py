8ad099f2a7f8630b4898d5636b953c99fb69eebf
imanolazpiroz17 <imanolazpiroz77@gmail.com>
2025-03-17 16:57:23 -0300

    Actividad 7.C
    Se modifico el programa para que no se acceda a las listas usando indices.

--- Practica 1/questions.py
+++ Practica 1/questions.py
@@ -29,27 +29,24 @@ puntaje_total = 0.0
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
-        print(f"{i + 1}. {answer}")
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+
+for question, options, correct_index in questions_to_ask:
+    print(question)
+    for i, option in enumerate(options, start = 1):
+        print(f"{i}) {option}")
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         try:
             user_answer = int(input("Respuesta: ")) - 1
-            if not (0 <= user_answer < len(answers[question_index])):
+            if not (0 <= user_answer < len(options)):
                 raise ValueError
         except ValueError:
             print("Respuesta no valida." , flush=True)
             sys.exit(1)
-            
-        # Se verifica si la respuesta es correcta
-        if user_answer == correct_answers_index[question_index]:
+
+        if (user_answer == correct_index):
             print("¡Correcto!")
             puntaje_total += 1.0
             break
@@ -58,8 +55,7 @@ for _ in range(3):
     else:
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correctas
-        print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(f"Incorrecto. La respuesta correcta es: {correct_index + 1}) {options[correct_index]}")
 
         # Se imprime un blanco al final de la pregunta
         print()
